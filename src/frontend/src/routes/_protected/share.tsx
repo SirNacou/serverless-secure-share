37756fd07aab4d096f2072e750f62cebdde18537
solus <quocanhthedev@gmail.com>
2026-06-20 00:25:19 +0700

    feat: add Tabs component with TabsList, TabsTrigger, and TabsContent for improved UI navigation

--- src/frontend/src/routes/_protected/share.tsx
+++ src/frontend/src/routes/_protected/share.tsx
@@ -1,3 +1,4 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "#/components/ui/tabs";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_protected/share")({
@@ -7,9 +8,37 @@ export const Route = createFileRoute("/_protected/share")({
 function RouteComponent() {
 	return (
 		<div className="h-full grid grid-cols-2 grid-rows-[1fr_auto] gap-4">
-			<div className="col-span-2 md:col-span-1">Left</div>
+			<div className="col-span-2 md:col-span-1">
+				<Input />
+			</div>
 			<div className="col-span-2 md:col-span-1">Right</div>
 			<div className="col-span-2">Bottom</div>
 		</div>
+	);
+}
+
+function Input() {
+	return (
+		<Tabs defaultValue="file">
+			<TabsList
+				className="w-full text-xl font-bold gap-0 rounded-none border-b p-0"
+				variant={"line"}
+			>
+				<TabsTrigger
+					className="data-active:border-primary! -mb-px rounded-none border! group-data-horizontal/tabs:after:bottom-[-0.5px]"
+					value="file"
+				>
+					File
+				</TabsTrigger>
+				<TabsTrigger
+					className="data-active:border-primary! -mb-px rounded-none border! group-data-horizontal/tabs:after:bottom-[-0.5px]"
+					value="text"
+				>
+					Text
+				</TabsTrigger>
+			</TabsList>
+			<TabsContent value="file">File</TabsContent>
+			<TabsContent value="text">Text</TabsContent>
+		</Tabs>
 	);
 }
